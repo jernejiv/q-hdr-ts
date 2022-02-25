78db77642b7447bf13806887a273dfe3ca52662f
Jernej Ivančič <jernej.ivancic@marand.si>
2022-02-25 14:07:08 +0100

    rework to setup to composition API not options

--- src/pages/Table.vue
+++ src/pages/Table.vue
@@ -3,10 +3,13 @@
     <q-btn label="load"
      @click="loadData"/>
 
+    {{ test }}
+    {{ test1 }}
+
     <q-page class="q-pa-sm">
       <q-card>
         <q-table title="List of home devices"
-                 :rows="deviceList"
+                 :rows="deviceList.data"
                  :columns="deviceTableColumns"
                  row-key="deviceName"
                  selection="single"
@@ -20,11 +23,11 @@
 <script lang="ts">
 import {api} from '../boot/axios';
 import {MyDevice} from '../components/types';
-import {defineComponent, ref} from 'vue';
+import {defineComponent, reactive, ref, toRefs} from 'vue';
 
 export default defineComponent ({
   // name: 'PageName',
-  data () {
+  /*data () {
     return { deviceList: ref([{} as MyDevice]),
       selectedDevice: ref([{} as MyDevice]),
       deviceTableColumns: [
@@ -49,8 +52,7 @@ export default defineComponent ({
       })
         .then((response) => {
           // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-          const result: MyDevice[] = response.data
-          this.deviceList = result
+          this.deviceList = response.data
         })
         .catch((error) => {
           console.log('napaka')
@@ -58,9 +60,52 @@ export default defineComponent ({
           console.dir(error)
         })
     }
-  },
+  },*/
   setup() {
-    // const selectedDevice = ref([])
+    let deviceList1 = ref([]);
+    let deviceList = reactive<{data: MyDevice[]}>({ data: [] });
+    console.log(deviceList.data);
+    console.log(toRefs(deviceList));
+    //let myDev: MyDevice = {id: '1', deviceName: 'device name'};
+    //console.log(myDev)
+    let test = 'bla';
+    let test1 = reactive({test})
+    // console.log(test1)
+
+
+    const deviceTableColumns = [
+      {
+        label: 'deviceName', field: 'deviceName', name: 'deviceName', align: 'left'
+      },
+      {
+        label: 'productNumber', field: 'productNumber', name: 'productNumber', align: 'left'
+      },
+      {
+        label: 'deviceType', field: 'deviceType', name: 'deviceType', align: 'left'
+      }
+    ]
+
+    function loadData(): void {
+      console.log('Load data executed')
+    }
+
+    api.get('/device-repository', {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        deviceList.data = response.data
+      })
+      .catch((error) => {
+        console.log('napaka')
+        // console.dir(state.deviceList)
+        console.dir(error)
+      })
+
+    return {deviceTableColumns, test, test1, deviceList , loadData, deviceList1}
   }
 })
 </script>
